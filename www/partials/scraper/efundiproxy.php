803adef0d2de0d3c429463da975eb7e5a4955731
WikusKriek <Wikus.Kriek.2w@gmail.com>
2019-09-07 19:37:10 +0200

    finished efundi scraper returns json with subjects,assignments,announcements and user info.

--- www/partials/scraper/efundiproxy.php
+++ www/partials/scraper/efundiproxy.php
@@ -7,7 +7,7 @@ $urlefundilogin = "https://casprd.nwu.ac.za/cas/login?service=http%3A%2F%2Fefund
 
 
 
-
+$student=array();
 
 
 //open connection
@@ -48,9 +48,45 @@ $result1 = curl_exec($ch);
 $html->load($result1);
 
 $anounurl=$html->find('a[title=Announcements - For posting current, time-critical information]',0)->href;
+$accounturl=$html->find('a[title=Account - View and modify my user profile]',0)->href;
+$starsubjects=$html->find('li[class=Mrphs-sitesNav__menuitem]');
+//for each subject in stared subjects get anouncement
+curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch,CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch,CURLOPT_POST, false);
+curl_setopt($ch,CURLOPT_COOKIEJAR, $cookie1);
+$array=array();
+$subjectnames=array();
+foreach($starsubjects as $subject) {
+      array_push($subjectnames,array('subject'=>$subject->find('span',0)->plaintext));
+      // push the cell's text to the array
+
+      $suburl= $subject->find('a',0)->href;
+
+      curl_setopt($ch,CURLOPT_URL, $suburl);
+      $result4 = curl_exec($ch);
+      $html= new simple_html_dom();
+      $html->load($result4);
+      $assignmenturl=$html->find('a[title=Assignments - For posting, submitting and grading assignment(s) online]',0)->href;
+      curl_setopt($ch,CURLOPT_URL, $assignmenturl);
+      $result5 = curl_exec($ch);
+      $html= new simple_html_dom();
+      $html->load($result5);
+      $new=$html->find('tr');
+      $array2=array();
+      foreach(array_slice($new,1) as $subject1){
+        $array2=array('subject'=>trim($subject->find('span',0)->plaintext),
+        "title"=> trim($subject1->find('a',0)->title),
+        'due'=>trim($subject1->find('td[headers=dueDate]',0)->plaintext),
+        'status'=>trim($subject1->find('td[headers=status]',0)->plaintext)
+        );
+        array_push($array,array("assignment"=>$array2));
+      }
 
 
-
+  }
+array_push($student,array('assignments'=>$array));
+array_push($student,array('subjects'=>$subjectnames));
 
 // found anouncement url now go there and scrape boy
 $fields = [
@@ -58,7 +94,6 @@ $fields = [
 'selectPageSize'=> 100
 ];
 $fields_string = http_build_query($fields);
-$anounurl=$anounurl;
 curl_setopt($ch,CURLOPT_URL, $anounurl);
 curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch,CURLOPT_FOLLOWLOCATION, true);
@@ -68,21 +103,34 @@ $result3 = curl_exec($ch);
 $html->load($result3);
 
 $fields2=$html->find('tr');
-echo '[';
+$announcements=array();
+$announcement=array();
 foreach(array_slice($fields2,1) as $row){
-  echo '{';
-$k=1;
-   echo $k.':'.$row->find('th',0)->find('span',0)->plaintext.',';
-   $k++;
-  foreach($row->find('td') as $cell) {
-        // push the cell's text to the array
-        echo $k.':'.$cell->plaintext.',';
-$k++;
-    }
-echo '},';
+
+   array_push($announcement,array("title"=>trim($row->find('th',0)->find('span',0)->plaintext),
+  "author"=>trim($row->find('td[headers=author]',0)->plaintext),
+  "date"=>trim($row->find('td[headers=date]',0)->plaintext),
+  "module"=>trim($row->find('td[headers=channel]',0)->plaintext)));
+
+}
+array_push($student,array("announcements"=>$announcement));
+//account details
+curl_setopt($ch,CURLOPT_URL, $accounturl);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch,CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch,CURLOPT_POST, false);
+curl_setopt($ch,CURLOPT_COOKIEJAR, $cookie1);
+$result3 = curl_exec($ch);
+$html->load($result3);
+$fields2=$html->find('div[class=shorttext]');
+$userinfo=array();
+foreach($fields2 as $row){
+  array_push($userinfo,array(trim($row->plaintext)));
 }
 
-echo ']';
+array_push($student,array("user"=>$userinfo));
+echo json_encode($student);
+//start assignment scraper
 
 curl_close($ch);
 
