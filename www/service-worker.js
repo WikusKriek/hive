19ff3bbf7ec83c11a1ba8a91f42bd712c168909a
WikusKriek <Wikus.Kriek.2w@gmail.com>
2020-01-16 16:04:58 +0200

    service worker

--- www/service-worker.js
+++ www/service-worker.js
@@ -50,9 +50,7 @@ workbox.routing.registerRoute(
 self.addEventListener('install', event => {
   self.skipWaiting();
 
-  event.waitUntil(
-    // caching etc
-  );
+
 });
 
 self.addEventListener('activate', event => {
@@ -60,9 +58,9 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(
       keys.map(key => {
-        if (!expectedCaches.includes(key)) {
+
           return caches.delete(key);
-        }
+
       })
     )).then(() => {
       console.log('V3 now ready to handle fetches!');
